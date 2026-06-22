3f0c96a8af22d0bee8e4b78d3852ed7b96416d29
Dhanush2805 <dhanushns2000@gmail.com>
2026-06-22 19:26:57 +0530

    Testing npm-audit

--- app.py
+++ app.py
@@ -2,20 +2,3 @@ print('success')
 print('success')
 print('success')
 print('success')
-
-from aws_cdk import (
-    Stack,
-    aws_s3 as s3
-)
-from constructs import Construct
-
-class BadStack(Stack):
-    def __init__(self, scope: Construct, id: str):
-        super().__init__(scope, id)
-
-        # ❌ FAIL: public bucket
-        s3.Bucket(
-            self,
-            "BadBucket",
-            public_read_access=True
-        )
