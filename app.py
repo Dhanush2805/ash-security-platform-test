e883e4957cdb48ff47a4f73720388c8ce51fed23
Dhanush2805 <dhanushns2000@gmail.com>
2026-06-22 18:11:09 +0530

    Testing grype

--- app.py
+++ app.py
@@ -3,9 +3,3 @@ print('success')
 print('success')
 print('success')
 
-import jwt
-
-token = jwt.decode(
-    token_string,
-    options={"verify_signature": False}
-)
--- bootstrap.ps1
+++ bootstrap.ps1
@@ -3,6 +3,11 @@ Write-Host "Installing Security Platform..."
 pip install pre-commit
 pip install git+https://github.com/awslabs/automated-security-helper.git@v3.5.4
 
+pip install semgrep
+
+# ensure ASH plugin dependency resolution is updated
+ash dependencies
+
 pre-commit install
 pre-commit install --hook-type pre-push
 
