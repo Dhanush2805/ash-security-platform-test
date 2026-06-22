722f20f2c2abec587ab43c679f643282836fc563
Dhanush2805 <dhanushns2000@gmail.com>
2026-06-22 18:30:29 +0530

    Testing cdk-nag

--- app.py
+++ app.py
@@ -3,3 +3,19 @@ print('success')
 print('success')
 print('success')
 
+from aws_cdk import (
+    Stack,
+    aws_s3 as s3
+)
+from constructs import Construct
+
+class BadStack(Stack):
+    def __init__(self, scope: Construct, id: str):
+        super().__init__(scope, id)
+
+        # ❌ FAIL: public bucket
+        s3.Bucket(
+            self,
+            "BadBucket",
+            public_read_access=True
+        )
