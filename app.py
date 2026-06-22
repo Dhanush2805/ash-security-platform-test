a1d9e981384aaf5031837f0e3fefc7f5127c26f0
Dhanush2805 <dhanushns2000@gmail.com>
2026-06-22 14:48:13 +0530

    Testing ASH

--- app.py
+++ app.py
@@ -2,3 +2,13 @@ print('success')
 print('success')
 print('success')
 print('success')
+
+
+import flask
+from flask import request
+
+app = flask.Flask(__name__)
+
+@app.route("/user")
+def user():
+    return request.args["name"]
