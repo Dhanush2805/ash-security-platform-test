775545c3911d00e0998e873ff8312f360af070aa
Dhanush2805 <dhanushns2000@gmail.com>
2026-06-22 14:51:21 +0530

    Testing ASH
    # Conflicts:
    #	app.py

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ print('success')
 print('success')
 
 
+<<<<<<< HEAD
 import requests
 
 requests.get(
     "https://example.com",
     verify=False
 )
+=======
+from flask import request
+name = request.args["name"]
+>>>>>>> b4e6869 (Testing ASH)
